9cd081bdd329f58134eb64d85b4f44d85147d120
8bitme <yairmark@gmail.com>
2016-01-05 23:19:17 +0200

    Moved oh-my-zsh into its own proper setup file (had to make it completion.zsh to force it to run last otherwise the zsh config overides it), gls does not alias ls nicely with oh-my-zsh for some reason need to see why

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,3 +1,5 @@
 # Install oh-my-zsh manually
-echo "› installing oh-my-zsh"
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+	echo "› installing oh-my-zsh"
+	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
