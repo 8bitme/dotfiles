93cacbc17033218c5f5a037bcc2780c84df439e8
Yair Mark <yair.mark@iwijhbanRFH05.local>
2016-01-23 01:54:12 +0200

    Added the colorize alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias reload!='. ~/.zshrc'
 alias c='clear'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias colorize="sed 's#WARN#\x1b[33m&#; s#ERROR#\x1b[31m&#; s#INFO#\x1b[32m&#'"
