312ed43f70ebaa243d0ec0f09653aedaa7843f70
8bitme <yairmark@gmail.com>
2016-04-17 20:08:33 +0200

    Disabled the annoying OSX update check every time a new shell is opened, added the cordova install for node

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,28 @@
-if test ! $(which spoof)
-then
-  sudo npm install spoof -g
+extract_packages_not_installed(){
+	local __extracted_packages=$2
+	local __packages_not_installed=$1
+
+	for line in $__packages_not_installed; do echo $line | awk '{print $NF}';done
+}
+
+run_npm() {
+local NPM_GLOBAL_PACKAGES="tldr spoof dark-mode pipeable-js jsome jshint cordova"
+if hash npm 2>/dev/null; then
+	local packages_not_installed=$( { hash $NPM_GLOBAL_PACKAGES } 2>&1  )
+	extract_packages_not_installed packages_not_installed packages_to_install
+
+	echo "Packages not installed $packages_not_installed"
+
+	if [ ! -z "$packages_to_install" ]
+	then
+   		echo "Installing nodejs global packages"
+		echo "Packages to install $packages_to_install"
+  		npm install $packages_to_install --global
+  		[[ $? ]] && echo "Successfully installed nodehs global packages"
+	fi
+else
+	echo "Error: npm not found"
+	exit
 fi
+}
+run_npm
