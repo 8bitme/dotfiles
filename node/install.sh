c9da20ac6da8989004fe1c55c22a6db5d1a6969b
Yair <yair.mark@iwijhbanRFH05.local>
2016-09-12 11:52:03 +0200

    Moved kdiff to the cask section as it is now a cask, added more aliases under zsh aliases

--- node/install.sh
+++ node/install.sh
@@ -5,24 +5,24 @@ extract_packages_not_installed(){
 	for line in $__packages_not_installed; do echo $line | awk '{print $NF}';done
 }
 
-run_npm() {
-local NPM_GLOBAL_PACKAGES="n tldr spoof dark-mode pipeable-js jsome jshint cordova ionic@beta commitizen"
-if hash npm 2>/dev/null; then
-	local packages_not_installed=$( { hash $NPM_GLOBAL_PACKAGES } 2>&1  )
-	extract_packages_not_installed packages_not_installed packages_to_install
-
-	echo "Packages not installed $packages_not_installed"
-
-	if [ ! -z "$packages_to_install" ]
-	then
-   		echo "Installing nodejs global packages"
-		echo "Packages to install $packages_to_install"
-  		npm install $packages_to_install --global
-  		[[ $? ]] && echo "Successfully installed nodehs global packages"
-	fi
-else
-	echo "Error: npm not found"
-	exit
-fi
-}
-run_npm
+#run_npm() {
+#local NPM_GLOBAL_PACKAGES="n tldr spoof dark-mode pipeable-js jsome jshint cordova ionic@beta commitizen"
+#if hash npm 2>/dev/null; then
+#	local packages_not_installed=$( { hash $NPM_GLOBAL_PACKAGES } 2>&1  )
+#	extract_packages_not_installed packages_not_installed packages_to_install
+#
+#	echo "Packages not installed $packages_not_installed"
+#
+#	if [ ! -z "$packages_to_install" ]
+#	then
+ #  		echo "Installing nodejs global packages"
+#		echo "Packages to install $packages_to_install"
+ # 		npm install $packages_to_install --global
+  #		[[ $? ]] && echo "Successfully installed nodehs global packages"
+#	fi
+#else
+#	echo "Error: npm not found"
+#	exit
+#fi
+#}
+#run_npm
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,70 @@ alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall 
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
 alias colorize="sed 's#WARN#\x1b[33m&#; s#ERROR#\x1b[31m&#; s#INFO#\x1b[32m&#'"
+
+# Most of the below are taken from http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
+#ls aliases
+alias ls='ls --color=auto'
+alias ll='ls -la'
+alias l.='ls -d .* --color=auto'
+
+# cd
+# get rid of command not found ##
+alias cd..='cd ..'
+ 
+## a quick way to get out of current directory ##
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+# grep
+# Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Encryption
+alias sha1='openssl sha1'
+
+# Mkdir
+alias mkdir='mkdir -pv'
+
+# Diff
+alias diff='colordiff'
+
+# Mount
+alias mount='mount |column -t'
+
+# History
+alias h='history'
+
+# Process Management
+alias j='jobs -l'
+
+# Nifty Set of new commands
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+# Network
+# Show open ports
+alias ports='netstat -tulanp'
+
+# Web server Debug
+# get web server headers #
+alias header='curl -I'
+  
+# find out if remote server supports gzip / mod_deflate or not #
+alias headerc='curl -I --compress'
+
+# wget
+alias wget='wget -c'
+
+## set some other defaults ##
+alias df='df -H'
+alias du='du -ch'
+
